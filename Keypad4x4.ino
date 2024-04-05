b87df306525c4f0bc232d08a25b02ae927237453
Lore Gobara <loregobara@gmail.com>
2024-04-05 10:23:55 -0300

    Keypad 4x4 code

---
+++ Keypad4x4.ino
@@ -0,0 +1,32 @@
+#include <Keypad.h> // Biblioteca do codigo
+
+const byte LINHAS = 4; // Linhas do teclado
+const byte COLUNAS = 4; // Colunas do teclado
+
+const char TECLAS_MATRIZ[LINHAS][COLUNAS] = { // Matriz de caracteres (mapeamento do teclado)
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+const byte PINOS_LINHAS[LINHAS] = {9, 8, 7, 6}; // Pinos de conexao com as linhas do teclado
+const byte PINOS_COLUNAS[COLUNAS] = {5, 4, 3, 2}; // Pinos de conexao com as colunas do teclado
+
+Keypad teclado_personalizado = Keypad(makeKeymap(TECLAS_MATRIZ), PINOS_LINHAS, PINOS_COLUNAS, LINHAS, COLUNAS); // Inicia teclado
+
+void setup() {
+  Serial.begin(9600); // Inicia porta serial
+}
+
+void loop() {
+
+  char leitura_teclas = teclado_personalizado.getKey(); // Atribui a variavel a leitura do teclado
+
+  if (leitura_teclas) { // Se alguma tecla foi pressionada
+    Serial.println(leitura_teclas); // Imprime a tecla pressionada na porta serial
+    Serial.println("OIOI");
+  }
+
+  
+}
